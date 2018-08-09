45a1a999d4cb6078d34aa1243a491c66a63f316c
Hello-HeKai <2412012958@qq.com>
2018-08-09 23:33:38 +0800

    code

---
+++ PKUSC2018/D2T1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+using namespace std;
+int main()
+{
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++) scanf("%d",&a[i]);
+	scanf("%d",&Q);
+	while (Q--)
+	{
+		scanf("%d%d%d",&L,&R,&x);
+
+	}
+	return 0;
+}
